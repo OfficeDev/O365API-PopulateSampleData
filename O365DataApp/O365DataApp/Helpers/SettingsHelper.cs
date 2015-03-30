1423fa0daa722fc55462fe6f2666ed3828f834cd
Rohit Agrawal <rohiagra@microsoft.com>
2015-03-30 16:53:23 -0700

    Add license
    Add license

--- O365DataApp/O365DataApp/Helpers/SettingsHelper.cs
+++ O365DataApp/O365DataApp/Helpers/SettingsHelper.cs
@@ -1,4 +1,20 @@
-﻿using System;
+﻿﻿//----------------------------------------------------------------------------------------------
+//    Copyright 2015 Microsoft Corporation
+//
+//    Licensed under the MIT License (MIT);
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//      http://mit-license.org/
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+//----------------------------------------------------------------------------------------------
+
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
